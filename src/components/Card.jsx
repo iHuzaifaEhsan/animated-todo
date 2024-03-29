8321ca8d57e44e05b379dc7f9c454b05726c9230
iHuzaifaEhsan <iamhuzaifaehsan@gmail.com>
2024-03-29 08:10:32 +0500

    first commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { FaRegFileAlt } from "react-icons/fa";
+import { LuDownload } from "react-icons/lu";
+import { IoMdClose } from "react-icons/io";
+import { motion } from "framer-motion";
+
+const Card = ({ data, reference }) => {
+  return (
+    <motion.div drag dragConstraints={reference} className="relative flex-shrink-0 w-60 h-72 rounded-[50px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden">
+      <FaRegFileAlt />
+      <p className="text-sm mt-5 leading-tight font-semibold">{data.desc}</p>
+      <div className="footer absolute bottom-0 w-full left-0">
+        <div className="flex items-center justify-between px-8 py-3 mb-3">
+          <h5>{data.filesize}</h5>
+          <span className="w-8 h-8 bg-zinc-600 rounded-full flex items-center justify-center">
+            {data.close ? (
+              <LuDownload size=".7em" color="#fff" />
+            ) : (
+              <IoMdClose />
+            )}
+          </span>
+        </div>
+        {data.tag.isOpen && (
+          <div
+            className={`tag w-full py-4 ${
+              data.tag.tagColor === "blue" ? "bg-blue-600" : "bg-green-600"
+            } flex items-center justify-center`}
+          >
+            <h3 className="text-sm font-semibold">{data.tag.tagTitle}</h3>
+          </div>
+        )}
+      </div>
+    </motion.div>
+  );
+};
+
+export default Card;
