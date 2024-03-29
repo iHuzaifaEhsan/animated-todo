20159e55e297ad5535598248b0e53dbb7544708a
iHuzaifaEhsan <iamhuzaifaehsan@gmail.com>
2024-03-29 11:22:02 +0500

    latest commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,13 @@ import { motion } from "framer-motion";
 
 const Card = ({ data, reference }) => {
   return (
-    <motion.div drag dragConstraints={reference} className="relative flex-shrink-0 w-60 h-72 rounded-[50px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden">
+    <motion.div
+      drag
+      dragConstraints={reference}
+      dragTransition={{ bounceStiffness: 500, bounceDamping: 30 }}
+      whileTap={{ scale: 0.9 }}
+      className="relative flex-shrink-0 w-60 h-72 rounded-[50px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden"
+    >
       <FaRegFileAlt />
       <p className="text-sm mt-5 leading-tight font-semibold">{data.desc}</p>
       <div className="footer absolute bottom-0 w-full left-0">
